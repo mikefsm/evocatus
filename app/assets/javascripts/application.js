5ff69cc8aeae51b0bf5fe25bd0548c740158229f
mikefsm <sftmfs@gmail.com>
2016-08-11 23:20:35 -0400

    Add materialize css, js, and fonts.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require materialize
+//= require materialize/extras/nouislider
+//= require init
---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('.modal-trigger').leanModal();
+    $(".dropdown-button").dropdown();
+    $('select').material_select();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
